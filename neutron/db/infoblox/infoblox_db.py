8fb7b2f142b0fdf9e76ad37924eaba6788f60e48
Nikolay Yakovlev <nyakovlev@mirantis.com>
2014-10-06 16:16:28 +0300

    OS-483, mutiple members for second network

--- neutron/db/infoblox/infoblox_db.py
+++ neutron/db/infoblox/infoblox_db.py
@@ -13,8 +13,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from sqlalchemy.orm import exc
-
 from neutron.db import external_net_db
 from neutron.db.infoblox import models
 from neutron.db import l3_db
@@ -31,16 +29,16 @@ def get_used_members(context, member_type):
     return [m.member_name for m in members]
 
 
-def get_member(context, map_id, member_type):
+def get_members(context, map_id, member_type):
     """Returns names of members used by currently used mapping (tenant id,
     network id or Infoblox netview name).
     """
     q = context.session.query(models.InfobloxMemberMap)
 
-    member = q.filter_by(map_id=map_id, member_type=member_type).first()
+    members = q.filter_by(map_id=map_id, member_type=member_type).all()
 
-    if member:
-        return member.member_name
+    if members:
+        return [member.member_name for member in members]
 
 
 def attach_member(context, map_id, member_name, member_type):
